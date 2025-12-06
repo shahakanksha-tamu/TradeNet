f4921c0e168397e07dde64a8d7c50752d4a405d3
Akanksha Shah <shahakanksha@tamu.edu>
2025-12-06 13:25:58 -0600

    add: agent class

--- tradenet/agent_ddqn.py
+++ tradenet/agent_ddqn.py
@@ -1,15 +1,2 @@
-# Online network
-
-# Target network
-
-# Double Q-learning update rule
-
-# Selection by online net
-
-# Evaluation by target net
-
-# Soft-update using tau
-
-# Epsilon-greedy exploration
-
-# Backprop training step
+class DDQNAgent:
+    pass
--- tradenet/agent_dqn.py
+++ tradenet/agent_dqn.py
@@ -1,9 +1,2 @@
-# ε-greedy policy
-
-# Q-network + target network
-
-# TD target computation
-
-# Experience replay sampling
-
-# Hard update of target network
+class DQNAgent:
+    pass
